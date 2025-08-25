ed8312abc3bf476904972a0fbce2a03fa5120ca7
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-08-25 15:09:57 +0000

    Trabajos vistos en la clase 3

--- Clase3/EjecutarLibro.java
+++ Clase3/EjecutarLibro.java
@@ -4,7 +4,7 @@ public class EjecutarLibro {
         Libro[] l = new Libro [7];
         l[0] = new Libro ("El Principito", "Antoine de Saint-Exupéry", 21500 );
         l[1] = new Libro ("Poesia completa", "Alejandra Pizarlik", 40000 );
-        l[2] = new Libro ("El Principito", "Antoine de Saint-Exupéry", 21500 );
+        l[2] = new Libro ("El Señor  de los anillos", "J. R. R. Tolkien", 60000 );
         l[3] = new Libro ("El Principito", "Antoine de Saint-Exupéry", 21500 );
         l[4] = new Libro ("El Principito", "Antoine de Saint-Exupéry", 21500 );
         l[5] = new Libro ("El Principito", "Antoine de Saint-Exupéry", 21500 );
