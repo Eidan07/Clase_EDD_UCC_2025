665d77e1bc3f19eb08169908372f7beab4689b88
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-09-15 14:37:10 +0000

    Se validó el uso de pilas

---
+++ Clase7/ValidacionCadenasPila.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class ValidacionCadenasPila {
+    
+    public static void main(String[] args) {
+        
+        String[] signos = {"{", "{", "}", "}","}"};
+
+        //Cree una pila de llaves y verifique si las llaves están
+        //completas, es decir, una llave que abre tiene una llave que cierra.
+        Stack<String> p = new Stack<>();
+        
+        for (int i = 0; i < signos.length; i++) {
+            p.push(signos[i]);
+        }
+
+        System.out.println(p);
+        int contador1 =0, contador2 = 0;
+        
+        int tam = p.size();
+        for (int i = 0; i < tam; i++) {
+            if(p.peek().equals("{")){
+                contador1 += 1;
+            }else{
+                contador2 += 1;
+            }
+            p.pop();
+        }
+
+        System.out.println(contador1==contador2 ? "Equilibrados" : "No Equilibrado");
+
+    }
+}
