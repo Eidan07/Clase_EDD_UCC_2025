2af3065f2e65149d407e4c771ae7f0080dbfaeff
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-09-22 13:39:34 +0000

    Creacion de la clase cola

---
+++ Clase8/Colas.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Colas {
+    
+    public static void main(String[] args) {
+    
+        Queue<Integer> cola = new ArrayDeque<>();
+
+        cola.add(5);
+        cola.add(10);
+        cola.add(3);
+        cola.add(20);
+
+        
+    }
+}
